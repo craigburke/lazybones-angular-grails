16c06f15c12b23506f6d309f08463b0cb81154b8
Craig Burke <craig@craigburke.com>
2014-09-26 10:01:15 -0400

    default war to ROOT.war

--- templates/angular-grails/lazybones.groovy
+++ templates/angular-grails/lazybones.groovy
@@ -9,7 +9,7 @@ params.appName = ask("Define the name for your project [angular-grails]: ", "ang
 params.group = ask("Define the value for your application group [com.company]: ", "com.company", "group")
 params.version = ask("Define value for your application 'version' [0.1]: ", "0.1", "version")
 params.angularModule = ask("Define value for your main AngularJS module [myApp]: ", "myApp", "angularModule")
-params.warName = ask("Define the name for your war file [${params.appName}.war]: ", "${params.appName}.war", "warName")
+params.warName = ask("Define the name for your war file [ROOT.war]: ", "ROOT.war", "warName")
 
 processTemplates 'gradle.properties', params
 processTemplates 'application.properties', params
