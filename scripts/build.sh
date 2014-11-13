ebaa9cec479f486d14f221438715ca3fcac61bcd
Craig Burke <craig@craigburke.com>
2014-11-13 10:08:25 -0500

    Combining setup and build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,14 @@
 #/bin/sh
 
+jdk_switcher use oraclejdk7
+
+# Install lazybones
+curl -s get.gvmtool.net | bash
+echo "gvm_auto_answer=true" > ~/.gvm/etc/config
+source ~/.gvm/bin/gvm-init.sh
+gvm install lazybones
+gvm use lazybones
+
 java -version
 
 # Test Angular 1.2
