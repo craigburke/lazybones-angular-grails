0b9663d752c3722b32b760f154eb8b46f1095ef2
Craig Burke <craig@craigburke.com>
2015-05-14 13:37:52 -0400

    Breaking out test and build into separate scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,29 +2,6 @@
 export PROJECT_ROOT=~/clone
 export APP_ROOT="$PROJECT_ROOT/test/app"
 
-jdk_switcher use oraclejdk7
-
-# Install lazybones
-curl -s get.gvmtool.net | bash
-echo "gvm_auto_answer=true" > ~/.gvm/etc/config
-source ~/.gvm/bin/gvm-init.sh
-gvm install lazybones
-gvm use lazybones
-
-java -version
-
-# Test Angular 1.2
-cd $PROJECT_ROOT
-./gradlew buildTestApp -PangularVersion=1.2
-cd $APP_ROOT
-./gradlew test
-
-# Test Angular 1.3
-cd $PROJECT_ROOT
-./gradlew buildTestApp -PangularVersion=1.3
-cd $APP_ROOT
-./gradlew test
-
 # Build
 cd $APP_ROOT
 ./gradlew assemble
