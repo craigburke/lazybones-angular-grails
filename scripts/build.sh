bc90f970afd0f726a0a19101918818c48df42e55
Craig Burke <craig@craigburke.com>
2015-05-14 14:40:59 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,4 @@ cd $PROJECT_ROOT
 ./gradlew buildTestApp -PangularVersion=1.3
 cd $APP_ROOT
 ./gradlew assemble
-mv build/libs/app-0.1.jar build/libs/angular-grails.jar
+mv build/libs/app-1.0.jar build/libs/angular-grails.jar
