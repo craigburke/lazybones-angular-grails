437494dbbf1f3407dd8987f76d8cd892ed694f4f
Craig Burke <craig@craigburke.com>
2015-05-14 13:39:57 -0400

    Make sure test app is built in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,9 @@
 export PROJECT_ROOT=~/clone
 export APP_ROOT="$PROJECT_ROOT/test/app"
 
-# Build
+# Build Angular 1.3
+cd $PROJECT_ROOT
+./gradlew buildTestApp -PangularVersion=1.3
 cd $APP_ROOT
 ./gradlew assemble
 mv build/libs/*.jar build/libs/angular-grails.jar
