7fb8ab5a20249eae74e8641e5b66ee6a29d6f113
Craig Burke <craig@craigburke.com>
2015-05-21 18:51:40 -0400

    Fixed edit page template so tests pass

--- templates/angular-grails/src/templates/angular/crud/src/test/groovy/_groupPath_/pages/_moduleName_/_resourceName_EditPage.groovy
+++ templates/angular-grails/src/templates/angular/crud/src/test/groovy/_groupPath_/pages/_moduleName_/_resourceName_EditPage.groovy
@@ -10,7 +10,7 @@ class ${resourceName}EditPage extends Page {
     static at = { \$('h2').text() == 'Edit ${resourceName}' }
 
     static content = {
-<%= domainProperties.collect {"\t\t\${it.name}Field {\$(\"${it.domainClass ? 'select' : 'input'}[ng-model='ctrl.${moduleName}.${it.name}']\")}" }.join('\n') %>
+<%= domainProperties.collect {"\t\t${it.name}Field {\$(\"${it.domainClass ? 'select' : 'input'}[ng-model='ctrl.${moduleName}.${it.name}']\")}" }.join('\n') %>
         saveButton { \$('button[crud-button="save"]') }
     }
 
