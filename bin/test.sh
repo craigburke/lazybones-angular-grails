79231ba2d20e2c9ee7f7b022905b8041b1789664
Craig Burke <craig@craigburke.com>
2015-05-22 06:59:44 -0400

    Correcting test app directory in script

--- bin/test.sh
+++ bin/test.sh
@@ -7,9 +7,9 @@ cd $PROJECT_ROOT
 ./gradlew buildTestApps
 
 # Test Angular 1.2
-cd $PROJECT_ROOT/test/app/1.2/
+cd $PROJECT_ROOT/test/apps/1.2/
 ./gradlew test
 
 # Test Angular 1.3
-cd $PROJECT_ROOT/test/app/1.3/
+cd $PROJECT_ROOT/test/apps/1.3/
 ./gradlew test
