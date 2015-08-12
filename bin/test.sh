a3985724855417ae2c3892f03220cd82cea75b19
Craig Burke <craig@craigburke.com>
2015-08-12 07:46:57 -0400

    CircleCI parrelism

--- bin/test.sh
+++ bin/test.sh
@@ -1,17 +1,23 @@
 #/bin/sh
-source ./bin/common.sh
+
+install_lazybones() {
+    curl -s get.gvmtool.net | bash
+    echo "gvm_auto_answer=true" > ~/.gvm/etc/config
+    source ~/.gvm/bin/gvm-init.sh
+    gvm install lazybones
+    gvm use lazybones
+}
+
+test_app() {
+    echo "Testing AngularJS $1 application"
+    local APP_PATH="./test/apps/$1"
+    $APP_PATH/gradlew -p $APP_PATH test
+}
 
 install_lazybones
-./gradlew buildTestApps
 
-# Test Angular 1.2
-cd $TEST_APP_ROOT/1.2
-./gradlew test
-
-# Test Angular 1.3
-cd $TEST_APP_ROOT/1.3
-./gradlew test
-
-# Test Angular 1.4
-cd $TEST_APP_ROOT/1.4
-./gradlew test
+case $CIRCLE_NODE_INDEX in 
+    0) test_app "1.2" ;;
+    1) test_app "1.3" ;;
+    2) test_app "1.4" ;;
+esac
